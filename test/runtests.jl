33c28e187f8efb1d529b5a5edfc4c96b883a9026
C.Brenhin Keller <cbkeller@dartmouth.edu>
2022-02-05 15:57:00 -0500

    Test @mm_string

--- test/runtests.jl
+++ test/runtests.jl
@@ -7,15 +7,20 @@ using ManualMemory: MemoryBuffer
     # Test StaticString constructors
     str = c"Hello, world! 🌍"
     @test isa(str, StaticString{19})
-    
+
+    # Test direct buffer constructor
+    buf = mm"Hello, world! 🌍"
+    @test isa(buf, MemoryBuffer{18, UInt8})
 
     # Test ascii escaping
     many_escapes = c"\0\a\b\f\n\r\t\v'\"\\"
     @test isa(many_escapes, StaticString{12})
     @test length(many_escapes) == 11
-    a = codeunits(many_escapes)
-    c = MemoryBuffer((codeunits("\0\a\b\f\n\r\t\v'\"\\\0")...,))
+    a = codeunits(many_escapes)::MemoryBuffer
+    b = mm"\0\a\b\f\n\r\t\v'\"\\\0"::MemoryBuffer
     @test a.data == b.data
+    c = MemoryBuffer((codeunits("\0\a\b\f\n\r\t\v'\"\\\0")...,))
+    @test b.data == c.data
 
 
 end
