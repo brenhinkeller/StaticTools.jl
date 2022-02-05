ae4cecb7728101f762056f25773ff12568cfbb07
C.Brenhin Keller <cbkeller@dartmouth.edu>
2022-02-05 15:52:27 -0500

    Initial tests for StaticString constructors

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,21 @@
 using StaticTools
 using Test
+using ManualMemory: MemoryBuffer
 
 @testset "StaticTools.jl" begin
-    # Write your tests here.
+
+    # Test StaticString constructors
+    str = c"Hello, world! 🌍"
+    @test isa(str, StaticString{19})
+    
+
+    # Test ascii escaping
+    many_escapes = c"\0\a\b\f\n\r\t\v'\"\\"
+    @test isa(many_escapes, StaticString{12})
+    @test length(many_escapes) == 11
+    a = codeunits(many_escapes)
+    c = MemoryBuffer((codeunits("\0\a\b\f\n\r\t\v'\"\\\0")...,))
+    @test a.data == b.data
+
+
 end
