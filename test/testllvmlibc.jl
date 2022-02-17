66d26f6756490eb8dee452a98a65c46a32f0397d
C.Brenhin Keller <cbkeller@dartmouth.edu>
2022-02-17 01:50:04 -0500

    test return value of memcpy

--- test/testllvmlibc.jl
+++ test/testllvmlibc.jl
@@ -5,6 +5,6 @@
     @test free(p) == 0
 
     a = MallocArray{Float64}(undef, 100)
-    StaticTools.memcpy!(a, ones(100))
+    @test StaticTools.memcpy!(a, ones(100)) == 0
     @test a == ones(100)
     free(a)
