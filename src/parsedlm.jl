5fe11677e63b49c5e930a1248bb82909fcccf3b9
C.Brenhin Keller <cbkeller@dartmouth.edu>
2022-04-24 15:57:05 -0400

    Try to add separate badge for nightly CI

--- src/parsedlm.jl
+++ src/parsedlm.jl
@@ -1,4 +1,37 @@
-# Parse a CSV file, return pointer to a 2d array of doubles
+
+"""
+```julia
+parsedlm([T::Type], filepath::String, delimiter::Char)
+```
+
+Parse a delimited text file, given a `filepath` and `delimiter`, and return the
+parsed contents as a `MallocMatrix{T}`, that is a 2D `MallocArray` containing
+numbers of type `T`.
+
+If not specified, the parse type `T` will default to `Float64`.
+
+## Examples
+```julia
+julia> using StaticTools
+
+julia> m = (1:10) * (1:10)';
+
+julia> fp = fopen(c"testfile.tsv", c"w"); printf(fp, m); fclose(fp);
+
+julia> parsedlm(Int32, c"testfile.tsv", '\t')
+10×10 MallocMatrix{Int32}:
+  1   2   3   4   5   6   7   8   9   10
+  2   4   6   8  10  12  14  16  18   20
+  3   6   9  12  15  18  21  24  27   30
+  4   8  12  16  20  24  28  32  36   40
+  5  10  15  20  25  30  35  40  45   50
+  6  12  18  24  30  36  42  48  54   60
+  7  14  21  28  35  42  49  56  63   70
+  8  16  24  32  40  48  56  64  72   80
+  9  18  27  36  45  54  63  72  81   90
+ 10  20  30  40  50  60  70  80  90  100
+```
+"""
 @inline parsedlm(filepath, delimiter::Char) = parsedlm(Float64, filepath, delimiter::Char)
 @inline function parsedlm(::Type{T}, filepath, delimiter::Char) where {T}
 
--- src/staticrng.jl
+++ src/staticrng.jl
@@ -2,14 +2,67 @@
 const Bits64 = Union{Int64, UInt64, Float64}
 abstract type StaticRNG end
 @inline Base.pointer(x::StaticRNG) = Ptr{UInt64}(Base.pointer_from_objref(x))
-@inline static_rng(seed=StaticTools.time()) = Xoshiro256✴︎✴︎(seed)
 
 # SplitMix64
+"""
+```julia
+SplitMix64([seed::Bits64])
+```
+Initialize the internal state of a StaticCompiler-safe (non-allocating) `SplitMix64`
+deterministic pseudorandom number generator, optionally specifying a 64-bit `seed`
+(which may be a `Float64`, `Int64`, or `UInt64`).
+
+If a seed is not specified, `StaticTools.time()` will be used, which returns
+the current Unix epoch time in seconds.
+
+### See also:
+`splitmix64`, `rand`
+
+## Examples
+```julia
+julia> seed = StaticTools.time() # Pick a seed
+1649890154
+
+julia> rng = SplitMix64(seed) # Initialize the generator
+SplitMix64{Int64}((1649890154,))
+
+julia> splitmix64(rng) # Draw a pseudorandom `UInt64` from the generator
+0xca764ac7b7ea31e8
+
+julia> rand(rng) # Draw a `Float64` between 0 and 1
+0.8704883051360292
+```
+"""
 mutable struct SplitMix64{T<:Bits64} <: StaticRNG
     state::NTuple{1,T}
 end
 @inline SplitMix64(seed::Bits64=StaticTools.time()) = SplitMix64((seed,))
 @inline Base.rand(rng::SplitMix64) = splitmix64(rng)/typemax(UInt64)
+"""
+```julia
+splitmix64([rng::SplitMix64])
+```
+A StaticCompiler-safe (non-allocating) implementation of the SplitMix64
+deterministic pseudorandom number generator.
+
+### See also:
+`SplitMix64`, `rand`
+
+## Examples
+```julia
+julia> seed = StaticTools.time() # Pick a seed
+1649890154
+
+julia> rng = SplitMix64(seed) # Initialize the generator
+SplitMix64{Int64}((1649890154,))
+
+julia> splitmix64(rng) # Draw a pseudorandom `UInt64` from the generator
+0xca764ac7b7ea31e8
+
+julia> rand(rng) # Draw a `Float64` between 0 and 1
+0.8704883051360292
+```
+"""
 @inline splitmix64(rng::SplitMix64=SplitMix64()) = GC.@preserve rng splitmix64(pointer(rng))
 @inline function splitmix64(state::Ptr{UInt64})
     s = unsafe_load(state)
@@ -22,15 +75,67 @@ end
 end
 
 # Xoshiro256✴︎✴︎
+"""
+```julia
+Xoshiro256✴︎✴︎(seed::NTuple{4,Bits64})
+```
+Initialize the internal state of a StaticCompiler-safe (non-allocating)
+`Xoshiro256✴︎✴︎` deterministic pseudorandom number generator, specifying a 256-bit
+`seed`, which should be specified as an `NTuple` of four 64-bit numbers (all
+either `Float64`, `Int64`, or `UInt64`).
+
+### See also:
+`xoshiro256✴︎✴︎`, `static_rng`, `rand`
+
+## Examples
+```julia
+julia> seed = (0x9b134eccd2e63538, 0xd74ab64b2c3ecc9b, 0x70ba9c07628c27bf, 0x270a2eb658e6130b)
+(0x9b134eccd2e63538, 0xd74ab64b2c3ecc9b, 0x70ba9c07628c27bf, 0x270a2eb658e6130b)
+
+julia> rng = Xoshiro256✴︎✴︎(seed) # Initialize the generator
+Xoshiro256✴︎✴︎{UInt64}((0x9b134eccd2e63538, 0xd74ab64b2c3ecc9b, 0x70ba9c07628c27bf, 0x270a2eb658e6130b))
+
+julia> xoshiro256✴︎✴︎(rng) # Draw a pseudorandom `UInt64` from the generator
+0x11059b6384fba06a
+
+julia> rand(rng) # Draw a `Float64` between 0 and 1
+0.9856766307398369
+```
+"""
 mutable struct Xoshiro256✴︎✴︎{T<:Bits64} <: StaticRNG
     state::NTuple{4,T}
 end
 @inline function Xoshiro256✴︎✴︎(seed::Bits64=time())
     rng = SplitMix64(seed)
-    Xoshiro256✴︎✴︎((rand(rng),rand(rng),rand(rng),rand(rng)))
+    Xoshiro256✴︎✴︎((splitmix64(rng),splitmix64(rng),splitmix64(rng),splitmix64(rng)))
 end
 @inline Base.rand(rng::Xoshiro256✴︎✴︎) = xoshiro256✴︎✴︎(rng)/typemax(UInt64)
 # Xoshiro256✴︎✴︎ PRNG implemented in LLVM IR
+"""
+```julia
+xoshiro256✴︎✴︎(rng::Xoshiro256✴︎✴︎)
+```
+A StaticCompiler-safe (non-allocating) implementation of the Xoshiro256✴︎✴︎
+deterministic pseudorandom number generator, written in LLVM IR and
+invoked via `llvmcall`.
+
+### See also:
+`Xoshiro256✴︎✴︎`, `static_rng`, `rand`
+
+## Examples
+```julia
+julia> seed = (0x9b134eccd2e63538, 0xd74ab64b2c3ecc9b, 0x70ba9c07628c27bf, 0x270a2eb658e6130b);
+
+julia> rng = Xoshiro256✴︎✴︎(seed) # Initialize the generator
+Xoshiro256✴︎✴︎{UInt64}((0x9b134eccd2e63538, 0xd74ab64b2c3ecc9b, 0x70ba9c07628c27bf, 0x270a2eb658e6130b))
+
+julia> xoshiro256✴︎✴︎(rng) # Draw a pseudorandom `UInt64` from the generator
+0x11059b6384fba06a
+
+julia> rand(rng) # Draw a `Float64` between 0 and 1
+0.9856766307398369
+```
+"""
 @inline xoshiro256✴︎✴︎(rng::Xoshiro256✴︎✴︎) = GC.@preserve rng xoshiro256✴︎✴︎(pointer(rng))
 @inline function xoshiro256✴︎✴︎(state::Ptr{UInt64})
     Base.llvmcall(("""
@@ -123,3 +228,31 @@ end
     attributes #0 = { noinline nounwind ssp uwtable }
     """, "next"), UInt64, Tuple{Ptr{UInt64}}, state)
 end
+
+
+"""
+```julia
+static_rng([seed::Bits64])
+```
+Initialize a StaticCompiler-safe (non-allocating) deterministic pseudorandom
+number generator, optionally specifying a 64-bit `seed` (which may be any 64-bit
+primitive numeric type -- that is, `Float64`, `Int64`, or `UInt64`).
+
+In particular, `static_rng` uses the specified `seed` value (or if not specified,
+the current result of `StaticTools.time()`) to initialize a simple `SplitMix64`
+generator, which is then in turn used to bootstrap the larger seed required for a
+`Xoshiro256✴︎✴︎` generator.
+
+## Examples
+```julia
+julia> rng = static_rng()
+Xoshiro256✴︎✴︎{UInt64}((0x2d4c7aa97cc1a621, 0x63460fc58ff25249, 0x81498572d44bd2ec, 0x2d4e96d3a7e9fdd2))
+
+julia> rand(rng) # Draw a `Float64` between 0 and 1
+0.6577585429879329
+
+julia> rand(rng)
+0.4711097758403277
+```
+"""
+@inline static_rng(seed=StaticTools.time()) = Xoshiro256✴︎✴︎(seed)
