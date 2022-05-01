ab563bb4d12958c57392cae14d75632dc545740b
C.Brenhin Keller <cbkeller@dartmouth.edu>
2022-05-01 14:13:48 -0400

    Attempt to add integration tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,23 +2,30 @@ using StaticTools
 using Test
 using ManualMemory: MemoryBuffer
 
+const GROUP = get(ENV, "GROUP", "All")
 
-@testset "IO" begin include("testllvmio.jl") end
-@testset "libc" begin include("testllvmlibc.jl") end
-@testset "Parsing" begin include("testparse.jl") end
-@testset "StaticString" begin include("teststaticstring.jl") end
-@testset "MallocString" begin include("testmallocstring.jl") end
-@testset "MallocArray" begin include("testmallocarray.jl") end
-@testset "StaticRNG" begin include("teststaticrng.jl") end
-@testset "MemoryBuffer" begin
+@static if GROUP == "Core" || GROUP == "All"
+    @testset "IO" begin include("testllvmio.jl") end
+    @testset "libc" begin include("testllvmlibc.jl") end
+    @testset "Parsing" begin include("testparse.jl") end
+    @testset "StaticString" begin include("teststaticstring.jl") end
+    @testset "MallocString" begin include("testmallocstring.jl") end
+    @testset "MallocArray" begin include("testmallocarray.jl") end
+    @testset "StaticRNG" begin include("teststaticrng.jl") end
+    @testset "MemoryBuffer" begin
 
-    # Test direct buffer constructor
-    buf = mm"Hello, world! 🌍"
-    @test isa(buf, MemoryBuffer{18, UInt8})
+        # Test direct buffer constructor
+        buf = mm"Hello, world! 🌍"
+        @test isa(buf, MemoryBuffer{18, UInt8})
 
-    # Test ascii escaping
-    a = mm"\0\a\b\f\n\r\t\v'\"\\\0"::MemoryBuffer
-    b = MemoryBuffer((codeunits("\0\a\b\f\n\r\t\v'\"\\\0")...,))
-    @test a.data == b.data
+        # Test ascii escaping
+        a = mm"\0\a\b\f\n\r\t\v'\"\\\0"::MemoryBuffer
+        b = MemoryBuffer((codeunits("\0\a\b\f\n\r\t\v'\"\\\0")...,))
+        @test a.data == b.data
 
+    end
+end
+
+@static if GROUP == "Integration" || GROUP == "All"
+    @testset "StaticCompiler" begin include("teststaticcompiler.jl") end
 end
