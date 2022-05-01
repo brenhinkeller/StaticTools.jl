bf7aa69daf7fee5871f6df9932313db21f7ac45d
C.Brenhin Keller <cbkeller@dartmouth.edu>
2022-05-01 16:07:51 -0400

    Run integration tests in tempdir

--- test/teststaticcompiler.jl
+++ test/teststaticcompiler.jl
@@ -2,55 +2,55 @@
 
 ## --- Times table:
 
-    function times_table(argc::Int, argv::Ptr{Ptr{UInt8}})
-        argc == 3 || return printf(stderrp(), c"Incorrect number of command-line arguments\n")
-        rows = parse(Int64, argv, 2)            # First command-line argument
-        cols = parse(Int64, argv, 3)            # Second command-line argument
+function times_table(argc::Int, argv::Ptr{Ptr{UInt8}})
+    argc == 3 || return printf(stderrp(), c"Incorrect number of command-line arguments\n")
+    rows = parse(Int64, argv, 2)            # First command-line argument
+    cols = parse(Int64, argv, 3)            # Second command-line argument
 
-        M = MallocArray{Int64}(undef, rows, cols)
-        @inbounds for i=1:rows
-           for j=1:cols
-               M[i,j] = i*j
-           end
-        end
-        printf(M)
-        free(M)
+    M = MallocArray{Int64}(undef, rows, cols)
+    @inbounds for i=1:rows
+       for j=1:cols
+           M[i,j] = i*j
+       end
     end
+    printf(M)
+    free(M)
+end
 
-    # Attempt to compile
-    path = compile_executable(times_table, (Int64, Ptr{Ptr{UInt8}}), "./")
-    @test isa(path, String)
-    # Attempt to run
-    println("5x5 times table:")
-    status = run(`$path 5 5`)
-    @test isa(status, Base.Process)
-    @test status.exitcode == 0
+# Attempt to compile
+path = compile_executable(times_table, (Int64, Ptr{Ptr{UInt8}}), tempdir())
+@test isa(path, String)
+# Attempt to run
+println("5x5 times table:")
+status = run(`$path 5 5`)
+@test isa(status, Base.Process)
+@test status.exitcode == 0
 
 ## --- Random number generation
 
-    function rand_matrix(argc::Int, argv::Ptr{Ptr{UInt8}})
-        argc == 3 || return printf(stderrp(), c"Incorrect number of command-line arguments\n")
-        rows = parse(Int64, argv, 2)            # First command-line argument
-        cols = parse(Int64, argv, 3)            # Second command-line argument
+function rand_matrix(argc::Int, argv::Ptr{Ptr{UInt8}})
+    argc == 3 || return printf(stderrp(), c"Incorrect number of command-line arguments\n")
+    rows = parse(Int64, argv, 2)            # First command-line argument
+    cols = parse(Int64, argv, 3)            # Second command-line argument
 
-        M = MallocArray{Float64}(undef, rows, cols)
-        rng = static_rng()
-        @inbounds for i=1:rows
-         for j=1:cols
-            M[i,j] = rand(rng)
-         end
-        end
-        printf(M)
-        free(M)
+    M = MallocArray{Float64}(undef, rows, cols)
+    rng = static_rng()
+    @inbounds for i=1:rows
+     for j=1:cols
+        M[i,j] = rand(rng)
+     end
     end
+    printf(M)
+    free(M)
+end
 
-    # Attempt to compile
-    path = compile_executable(rand_matrix, (Int64, Ptr{Ptr{UInt8}}), "./")
-    @test isa(path, String)
-    # Attempt to run
-    println("5x5 random matrix:")
-    status = run(`$path 5 5`)
-    @test isa(status, Base.Process)
-    @test status.exitcode == 0
+# Attempt to compile
+path = compile_executable(rand_matrix, (Int64, Ptr{Ptr{UInt8}}), tempdir())
+@test isa(path, String)
+# Attempt to run
+println("5x5 random matrix:")
+status = run(`$path 5 5`)
+@test isa(status, Base.Process)
+@test status.exitcode == 0
 
 ## ---
