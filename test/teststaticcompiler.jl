d13398e4905cc58e9176f87c7101adc712c5da0b
C.Brenhin Keller <cbkeller@dartmouth.edu>
2022-05-02 00:58:33 -0400

    Clean up temporary files or redirect to tempdir

--- test/teststaticcompiler.jl
+++ test/teststaticcompiler.jl
@@ -1,7 +1,12 @@
+# Setup
+testpath = pwd()
+scratch = tempdir()
+cd(scratch)
+
 ## --- Times table:
 
 # Attempt to compile
-status = run(`julia -i times_table.jl`)
+status = run(`julia -i $testpath/times_table.jl`)
 @test isa(status, Base.Process)
 @test status.exitcode == 0
 
@@ -14,7 +19,7 @@ status = run(`./times_table 5 5`)
 ## --- Random number generation
 
 # Attempt to compile
-status = run(`julia -i rand_matrix.jl`)
+status = run(`julia -i $testpath/rand_matrix.jl`)
 @test isa(status, Base.Process)
 @test status.exitcode == 0
 
@@ -25,3 +30,5 @@ status = run(`./rand_matrix 5 5`)
 @test status.exitcode == 0
 
 ## ---
+
+cd(testpath)
