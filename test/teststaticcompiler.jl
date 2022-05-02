5b89aad885df3041753088e1a61989bfd3b63b58
C.Brenhin Keller <cbkeller@dartmouth.edu>
2022-05-02 00:51:04 -0400

    Hack to get integration tests running by spawning interactive julia processes

--- test/teststaticcompiler.jl
+++ test/teststaticcompiler.jl
@@ -1,55 +1,26 @@
-   using StaticCompiler
-
 ## --- Times table:
 
-function times_table(argc::Int, argv::Ptr{Ptr{UInt8}})
-    argc == 3 || return printf(stderrp(), c"Incorrect number of command-line arguments\n")
-    rows = parse(Int64, argv, 2)            # First command-line argument
-    cols = parse(Int64, argv, 3)            # Second command-line argument
-
-    M = MallocArray{Int64}(undef, rows, cols)
-    @inbounds for i=1:rows
-       for j=1:cols
-           M[i,j] = i*j
-       end
-    end
-    printf(M)
-    free(M)
-end
-
 # Attempt to compile
-path = compile_executable(times_table, (Int64, Ptr{Ptr{UInt8}}), tempdir())
-@test isa(path, String)
+status = run(`julia -i times_table.jl`)
+@test isa(status, Base.Process)
+@test status.exitcode == 0
+
 # Attempt to run
 println("5x5 times table:")
-status = run(`$path 5 5`)
+status = run(`./times_table 5 5`)
 @test isa(status, Base.Process)
 @test status.exitcode == 0
 
 ## --- Random number generation
 
-function rand_matrix(argc::Int, argv::Ptr{Ptr{UInt8}})
-    argc == 3 || return printf(stderrp(), c"Incorrect number of command-line arguments\n")
-    rows = parse(Int64, argv, 2)            # First command-line argument
-    cols = parse(Int64, argv, 3)            # Second command-line argument
-
-    M = MallocArray{Float64}(undef, rows, cols)
-    rng = static_rng()
-    @inbounds for i=1:rows
-     for j=1:cols
-        M[i,j] = rand(rng)
-     end
-    end
-    printf(M)
-    free(M)
-end
-
 # Attempt to compile
-path = compile_executable(rand_matrix, (Int64, Ptr{Ptr{UInt8}}), tempdir())
-@test isa(path, String)
+status = run(`julia -i rand_matrix.jl`)
+@test isa(status, Base.Process)
+@test status.exitcode == 0
+
 # Attempt to run
 println("5x5 random matrix:")
-status = run(`$path 5 5`)
+status = run(`./rand_matrix 5 5`)
 @test isa(status, Base.Process)
 @test status.exitcode == 0
 
